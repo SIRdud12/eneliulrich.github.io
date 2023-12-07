c1754b293ca74853e8aeb2cf91be22610e02d991
SIRdud12 <152188134+SIRdud12@users.noreply.github.com>
2023-12-07 06:03:12 +0100

    Add files via upload

---
+++ header.h
@@ -0,0 +1,14 @@
+//
+// Created by ENELI ENELI ULRICH YANN ALAN on 05/12/2023.
+//
+
+#ifndef DEBUT_PROJET_HEADER_H
+#define DEBUT_PROJET_HEADER_H
+
+#include <stdio.h>
+
+void remplirtableau(char tab[15][15]);
+
+void affichertab(char tab[15][15]);
+
+#endif //DEBUT_PROJET_HEADER_H
---
+++ main.c
@@ -0,0 +1,11 @@
+#include "header.h"
+
+int main() {
+    char tab[15][15];
+
+
+    remplirtableau(tab);
+    affichertab(tab);
+
+    return 0;
+}
